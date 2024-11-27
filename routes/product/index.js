5b03a6048ee911a43d0283857bf2c068b0edd9a1
Sunidhi <sunidhi@trigvent.com>
2024-11-27 15:32:56 +0530

    CRUD- Done

--- routes/product/index.js
+++ routes/product/index.js
@@ -2,6 +2,7 @@ import Router from "express-promise-router";
 import get from "./get/get.js";
 import post from "./post/post.js";
 import _delete from "./delete/delete.js";
+import update from "../update/update.js";
 import extractParam from "../../middleware/extractParams.js";
 
 const router = Router();
@@ -9,5 +10,8 @@ const router = Router();
 router.post("/", post);
 router.get("/", get);
 router.delete("/:productId", extractParam("productId"), _delete);
+
+// update functionality
+router.put("/:productId", extractParam("productId"), update);
 
 export default router;
